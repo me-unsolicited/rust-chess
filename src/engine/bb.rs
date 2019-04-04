cab7a456907b5af60b3b1dd6f19764b0dbb1e1e3
benmannon <benmannon@gmail.com>
2019-04-04 16:26:36 -0400

    Add bitboard tables for knight moves

--- src/engine/bb.rs
+++ src/engine/bb.rs
@@ -3,6 +3,7 @@ const NO_MOVE: u64 = 0;
 lazy_static! {
     pub static ref PAWN_MOVES: [u64; 64] = init_pawn_moves();
     pub static ref PAWN_ATTACKS: [u64; 64] = init_pawn_attacks();
+    pub static ref KNIGHT_MOVES: [u64; 64] = init_knight_moves();
 }
 
 fn init_pawn_moves() -> [u64; 64] {
@@ -55,6 +56,70 @@ fn init_pawn_attack(sq: usize) -> u64 {
     }
 
     left | right
+}
+
+fn init_knight_moves() -> [u64; 64] {
+    let mut moves = [u64; 64];
+    for sq in 0..64 {
+        moves[sq] = init_knight_move(sq);
+    }
+
+    moves
+}
+
+
+fn init_knight_move(sq: usize) -> u64 {
+    let (rank, file) = to_rank_file(sq);
+
+    // like clock hands; get it?
+    //
+    // |  |11|  | 1|  |
+    // |10|  |  |  | 2|
+    // |  |  | N|  |  |
+    // | 8|  |  |  | 4|
+    // |  | 7|  | 5|  |
+
+    let mut one = NO_MOVE;
+    if rank < 6 && file < 7 {
+        one = to_bit(rank + 2, file + 1)
+    }
+
+    let mut two = NO_MOVE;
+    if rank < 7 && file < 6 {
+        two = to_bit(rank + 1, file + 2)
+    }
+
+    let mut four = NO_MOVE;
+    if rank > 0 && file < 6 {
+        four = to_bit(rank - 1, file + 2)
+    }
+
+    let mut five = NO_MOVE;
+    if rank > 1 && file < 7 {
+        five = to_bit(rank - 2, file + 1)
+    }
+
+    let mut seven = NO_MOVE;
+    if rank > 1 && file > 0 {
+        seven = to_bit(rank - 2, file - 1)
+    }
+
+    let mut eight = NO_MOVE;
+    if rank > 0 && file > 1 {
+        eight = to_bit(rank - 1, file - 2)
+    }
+
+    let mut ten = NO_MOVE;
+    if rank < 7 && file > 1 {
+        ten = to_bit(rank + 1, file - 2)
+    }
+
+    let mut eleven = NO_MOVE;
+    if rank < 6 && file > 0 {
+        eleven = to_bit(rank + 2, file - 1)
+    }
+
+    one | two | four | five | seven | eight | ten | eleven
 }
 
 fn to_rank_file(sq: usize) -> (usize, usize) {

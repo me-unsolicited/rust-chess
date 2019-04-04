60d12ff545ad788f640651cf6c7c88581a947560
benmannon <benmannon@gmail.com>
2019-04-04 18:14:03 -0400

    Put bound check inside to_bit function

--- src/engine/bb.rs
+++ src/engine/bb.rs
@@ -45,15 +45,8 @@ fn init_pawn_attack(sq: usize) -> u64 {
         return NO_MOVE;
     }
 
-    let mut left = NO_MOVE;
-    if file > 0 {
-        left = to_bit(rank + 1, file - 1);
-    }
-
-    let mut right = NO_MOVE;
-    if right < 7 {
-        right = to_bit(rank + 1, file + 1);
-    }
+    let left = to_bit(rank + 1, file - 1);
+    let right = to_bit(rank + 1, file + 1);
 
     left | right
 }
@@ -79,45 +72,14 @@ fn init_knight_move(sq: usize) -> u64 {
     // | 8|  |  |  | 4|
     // |  | 7|  | 5|  |
 
-    let mut one = NO_MOVE;
-    if rank < 6 && file < 7 {
-        one = to_bit(rank + 2, file + 1)
-    }
-
-    let mut two = NO_MOVE;
-    if rank < 7 && file < 6 {
-        two = to_bit(rank + 1, file + 2)
-    }
-
-    let mut four = NO_MOVE;
-    if rank > 0 && file < 6 {
-        four = to_bit(rank - 1, file + 2)
-    }
-
-    let mut five = NO_MOVE;
-    if rank > 1 && file < 7 {
-        five = to_bit(rank - 2, file + 1)
-    }
-
-    let mut seven = NO_MOVE;
-    if rank > 1 && file > 0 {
-        seven = to_bit(rank - 2, file - 1)
-    }
-
-    let mut eight = NO_MOVE;
-    if rank > 0 && file > 1 {
-        eight = to_bit(rank - 1, file - 2)
-    }
-
-    let mut ten = NO_MOVE;
-    if rank < 7 && file > 1 {
-        ten = to_bit(rank + 1, file - 2)
-    }
-
-    let mut eleven = NO_MOVE;
-    if rank < 6 && file > 0 {
-        eleven = to_bit(rank + 2, file - 1)
-    }
+    let one = to_bit(rank + 2, file + 1);
+    let two = to_bit(rank + 1, file + 2);
+    let four = to_bit(rank - 1, file + 2);
+    let five = to_bit(rank - 2, file + 1);
+    let seven = to_bit(rank - 2, file - 1);
+    let eight = to_bit(rank - 1, file - 2);
+    let ten = to_bit(rank + 1, file - 2);
+    let eleven = to_bit(rank + 2, file - 1);
 
     one | two | four | five | seven | eight | ten | eleven
 }
@@ -127,5 +89,10 @@ fn to_rank_file(sq: usize) -> (usize, usize) {
 }
 
 fn to_bit(rank: usize, file: usize) -> u64 {
+
+    if rank < 0 || rank > 7 || file < 0 || file > 7 {
+        return NO_MOVE;
+    }
+
     1 << (u64)(rank * 8 + file)
 }

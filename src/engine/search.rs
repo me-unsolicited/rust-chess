89b153956cb556cbb99276158f9d1e1e32bed725
benmannon <benmannon@gmail.com>
2019-04-08 20:43:06 -0400

    Add alpha beta pruning

--- src/engine/search.rs
+++ src/engine/search.rs
@@ -5,8 +5,12 @@ use crate::engine::{EngineState, eval, gen, GoParams};
 use crate::engine::board::{Board, Color};
 use crate::engine::mov::Move;
 
-// min value that won't overflow on negation
+// min/max values that won't overflow on negation
 const MIN_EVAL: i32 = -std::i32::MAX;
+const MAX_EVAL: i32 = -MIN_EVAL;
+
+// compile-time configuration of search algorithm
+fn new_searcher() -> impl Searcher { NegamaxAb::new() }
 
 pub fn search(state: Arc<Mutex<EngineState>>, p: GoParams) {
     let root_position = state.lock().unwrap().position;
@@ -16,7 +20,7 @@ pub fn search(state: Arc<Mutex<EngineState>>, p: GoParams) {
         position = position.update(mov);
     }
 
-    let mut searcher = Negamax::new();
+    let mut searcher = new_searcher();
     let mov = searcher.search(position);
 
     let stats = searcher.get_stats();
@@ -83,6 +87,7 @@ impl Searcher for Negamax {
 impl Negamax {
     const DEPTH: i32 = 5;
 
+    #[allow(dead_code)]
     pub fn new() -> Self {
         Self {
             stats: SearchStats::new()
@@ -122,6 +127,85 @@ impl Negamax {
             if best_move.is_none() || eval > best_eval {
                 best_eval = eval;
                 best_move = Some(mov);
+            }
+        }
+
+        (best_eval, best_move)
+    }
+}
+
+struct NegamaxAb {
+    stats: SearchStats,
+}
+
+impl Searcher for NegamaxAb {
+    fn search(&mut self, position: Board) -> Move {
+        let start = Instant::now();
+
+        let sign = if position.turn == Color::WHITE { 1 } else { -1 };
+        let alpha = sign * MIN_EVAL;
+        let beta = sign * MAX_EVAL;
+        let (_, mov) = self.negamax(position, Self::DEPTH, alpha, beta, sign);
+
+        self.stats.time_elapsed = start.elapsed();
+
+        mov.expect("no move")
+    }
+
+    fn get_stats(&self) -> SearchStats {
+        self.stats
+    }
+}
+
+impl NegamaxAb {
+    const DEPTH: i32 = 7;
+
+    #[allow(dead_code)]
+    pub fn new() -> Self {
+        Self {
+            stats: SearchStats::new()
+        }
+    }
+
+    fn negamax(&mut self, position: Board, depth: i32, mut alpha: i32, beta: i32, sign: i32) -> (i32, Option<Move>) {
+
+        // track search statistics
+        self.stats.nodes_visited += 1;
+        self.stats.max_depth = self.stats.max_depth.max(Self::DEPTH - depth);
+
+        // have we reached max depth?
+        if depth <= 0 {
+            return (sign * eval::evaluate(&position), None);
+        }
+
+        let moves = gen::gen_moves(&position);
+
+        // no available moves? the game is over
+        if moves.is_empty() {
+            let perspective = if position.turn == Color::WHITE { position } else { position.mirror() };
+            let is_mate = !0 != gen::get_check_restriction(&perspective);
+            return (sign * if is_mate { MIN_EVAL } else { 0 }, None);
+        }
+
+        // choose the best variation
+        let mut best_eval = MIN_EVAL;
+        let mut best_move = None;
+
+        // go deeper for each move
+        for mov in moves {
+            let moved = position.update(mov);
+            let (eval, _) = self.negamax(moved, depth - 1, -beta, -alpha, -sign);
+            let eval = -eval;
+
+            if best_move.is_none() || eval > best_eval {
+                best_eval = eval;
+                best_move = Some(mov);
+            }
+
+            // alpha-beta pruning
+            alpha = alpha.max(best_eval);
+            if alpha >= beta {
+                break;
             }
         }
 
